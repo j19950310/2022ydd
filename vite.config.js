924d7aafbf7adaedd09558a71319b7c12f25ce0f
Jay <jaywu.fe@gmail.com>
2022-08-10 21:45:58 +0800

    [test] Test Base Url

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import path from "path";
 
+const OnlineUrl = 'https://j19950310.github.io/2022ydd/'
+const PROD = process.env.MODE === 'PROD'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: PROD ? OnlineUrl : undefined,
   plugins: [vue(),Pages({
     dirs: './src/views',
     exclude: ['**/components/*.vue'],
