8cc52673698004f73219845b0e253dafae3f7c24
Jay <jaywu.fe@gmail.com>
2022-08-22 23:29:44 +0800

    template replace router meta

--- copyRoute.js
+++ copyRoute.js
@@ -1,22 +1,67 @@
 import fs from 'fs'
 
 const list = [
-  ['./dist', './dist/topic'],
-  ['./dist', './dist/topic/yunlin-puppet-theater'],
+  [ // HOME
+    './dist',
+    './dist',
+    {
+      TITLE: "雲的觀察日誌|首頁",
+      URL: '/',
+      DESC: '首頁',
+      IMAGE: '/assets/img_slogan.4962dfee.png'
+    }
+  ],
+  [
+    './dist',
+    './dist/topic',
+    {
+      TITLE: "雲的觀察日誌|精選文章",
+      URL: '/topic',
+      DESC: '精選文章',
+      IMAGE: '/assets/img_slogan.4962dfee.png'
+    }
+  ],
+  [
+    './dist',
+    './dist/topic/yunlin-puppet-theater',
+    {
+      TITLE: "雲的觀察日誌|雲林文化探索#文化歷史建築】五、六年級生的回憶！掌中戲之鄉虎尾必訪特色景點「雲林布袋戲館」",
+      URL: '/topic',
+      DESC: '精選文章',
+      IMAGE: '/assets/img_slogan.4962dfee.png'
+    }
+  ],
 ]
+const regexList = [
+  [/{{URL}}/g, 'URL'],
+  [/{{DESC}}/g, 'DESC'],
+  [/{{IMAGE}}/g, 'IMAGE'],
+  [/{{TITLE}}/g, 'TITLE']
+]
+// console.log(string.replace(/-/g, ''))  
+
 const checkDir = (dir) => {
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
   }
 }
 
-
-for (let i = 0; i < list.length; i++) {
-  const [from, to] = list[i]
-  checkDir(to)
-
-  fs.copyFile(`${from}/index.html`, `${to}/index.html`, (err) => {
-    if (err) throw err;
-    console.log('source.txt was copied to destination.txt');
-  });
+const init = async () => {
+  for (let i = 0; i < list.length; i++) {
+    const [from, to, meta] = list[i]
+    const fromFile = `${from}/index.html`
+    const toFile = `${to}/index.html`
+    checkDir(to)
+  
+    await fs.copyFileSync(fromFile, toFile);
+  
+    let content = await fs.readFileSync(toFile, 'utf-8')
+    for (let index = 0; index < regexList.length; index++) {
+      const [regex, key] = regexList[index];
+      content = content.replace(regex, meta[key]);
+    }
+    fs.writeFileSync(toFile, content, 'utf-8');
+  }
 }
+
+init()
