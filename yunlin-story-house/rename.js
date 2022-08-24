e2f848a1a21d981fe9d30f87724a3517e3456e53
j19950310 <47713149+j19950310@users.noreply.github.com>
2022-08-24 18:29:36 +0000

    Deploying to gh-page from @ j19950310/2022ydd@7fbd9462ae5bc22585d1b78e04cde57efeec2752 🚀

--- yunlin-story-house/rename.js
+++ yunlin-story-house/rename.js
@@ -4,7 +4,7 @@ fs.readdir('.', (err, files) => {
     const [name, jpg] = file.split('.')
     console.log({name, jpg})
     if(jpg === 'jpg') {
-      fs.rename(file, 'yunlin_puppet_theater_'+`${name}`.padStart(2,'0')+'.jpg', function(err) {
+      fs.rename(file, 'yunlin_story_house'+`${name}`.padStart(2,'0')+'.jpg', function(err) {
           if ( err ) console.log('ERROR: ' + err);
       });
     }
